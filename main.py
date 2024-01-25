324c08068ce8e2e2438fa32ee89dc37652827478
Konstantin Wagner <94897208+wagkob20@users.noreply.github.com>
2024-01-25 14:38:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import discord
 from discord.ext import commands
-from datetime import timedelta
+from datetime import datetime, timedelta
+
+intents = discord.Intents.default()
+intents.messages = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+import discord
+from discord.ext import commands
+from datetime import datetime, timedelta
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -15,31 +24,38 @@ async def on_message(message):
         return
 
     user_id = message.author.id
-    message_content = message.content  
-    channel_id = message.channel.id  
-    message_id = message.id  
+    message_content = message.content
+    channel_id = message.channel.id
+    message_id = message.id
+    timestamp = datetime.now()
 
     key = (user_id, message_content)
 
-    if key in last_messages and last_messages[key][0] != channel_id:
-        log_channel = bot.get_channel("logchannelid-i-love-uiux")
-        if log_channel:
-            await log_channel.send(f"User {message.author} (ID: {user_id}) hat die Nachricht '{message_content}' in mehreren Kanälen gesendet.")
+    if key in last_messages:
+        last_channel_id, last_message_id, last_timestamp = last_messages[key]
 
-        await message.author.timeout(timedelta(minutes=5), reason="Wiederholte Nachricht in verschiedenen Kanälen")
-        
-        other_channel_id, other_message_id = last_messages[key]
-        other_channel = bot.get_channel(other_channel_id)
+        if last_channel_id != channel_id:
+            log_channel_id = "channel_id"  
+            log_channel = bot.get_channel(int(log_channel_id))
+            if log_channel:
+                await log_channel.send(f"User {message.author} (ID: {user_id}) hat die Nachricht '{message_content}' in mehreren Kanälen gesendet.")
+            
+            await message.author.timeout(timedelta(minutes=5), reason="Wiederholte Nachricht in verschiedenen Kanälen")
+            
+            other_channel = bot.get_channel(last_channel_id)
+            if other_channel:
+                try:
+                    other_message = await other_channel.fetch_message(last_message_id)
+                    await other_message.delete()
+                except discord.NotFound:
+                    pass
+            await message.delete()
 
-        if other_channel:
-            try:
-                other_message = await other_channel.fetch_message(other_message_id)
-                await other_message.delete()
-            except discord.NotFound:
-                pass 
-        await message.channel.fetch_message(message_id)
-        await message.delete()
-    else:
-        last_messages[key] = (channel_id, message_id)
+        elif channel_id == last_channel_id and (timestamp - last_timestamp) < timedelta(minutes=1):
+            await message.delete()
+            return
 
-bot.run("token-i-love-völk")
+    last_messages[key] = (channel_id, message_id, timestamp)
+
+
+bot.run("bot_token")
